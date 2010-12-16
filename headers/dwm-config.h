ac1d989a0eea932d31ba3263af81171e41d34f63
Jack Mottram <j@ck.mottr.am>
2010-12-16 20:49:49 +0000

    Cleaned up aliases file

--- headers/dwm-config.h
+++ headers/dwm-config.h
@@ -14,7 +14,7 @@ static const Bool showbar           = False;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "web", "vim", "term", "video", "5", "6", "7", "8", "9"};
+static const char *tags[] = { "1", "2", "3", "3", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
