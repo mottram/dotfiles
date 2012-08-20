132e422b12ad84a9be6800fd5b20313c0f35e889
Jack Mottram <j@ck.mottr.am>
2012-08-20 20:12:11 +0100

    Added chromium rule to dwm/config.h

--- dwm/config.h
+++ dwm/config.h
@@ -22,6 +22,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Chromium", NULL,       NULL,       1,            False,       -1 },
 };
 
 /* layout(s) */
