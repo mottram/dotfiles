4471cad7c7aad71558ab2897eb71eaeb5cb2d787
Jack Mottram <j@ck.mottr.am>
2016-07-20 08:57:31 +0100

    zsh: removed fzf-marks

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,8 +1,6 @@
 source $ZSH_PLUGINS_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 source $ZSH_PLUGINS_DIR/zsh-history-substring-search/zsh-history-substring-search.zsh
-source $ZSH_PLUGINS_DIR/fzf-marks/fzf-marks.plugin.zsh
-bindkey '^j' jump
 source $ZSH_PLUGINS_DIR/calc/calc.plugin.zsh
 if [[ $IS_OSX -eq 1 ]]; then
     source $ZSH_PLUGINS_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
